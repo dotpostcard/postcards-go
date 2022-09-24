60f84f7ce15d5e57373ada48d60f74652cc434c0
JP Hastings-Spital <jphastings@gmail.com>
2022-09-24 08:34:24 +0100

    Add simple test for thumbnailing

--- thumbnail.go
+++ thumbnail.go
@@ -6,25 +6,48 @@ import (
 
 	"github.com/jphastings/postcard-go/internal/types"
 	"golang.org/x/image/draw"
+	_ "golang.org/x/image/webp"
 )
 
+// Thumbnail generates a thumbnail image representing this postcard's front. Providing a maximum width or height of zero will allow
+// the thumbnail to grow up to the postcard's original size in that dimension.
 func Thumbnail(pc *types.Postcard, maxWidth, maxHeight int) (image.Image, error) {
 	img, _, err := image.Decode(bytes.NewReader(pc.Front))
 	if err != nil {
 		return nil, err
 	}
 
-	ratio := maxWidth / img.Bounds().Dx()
-	newWidth := maxWidth
-	newHeight := img.Bounds().Dy() * ratio
-
-	if newHeight > maxHeight {
-		newHeight = maxHeight
-		newWidth = img.Bounds().Dx() * maxHeight / img.Bounds().Dy()
-	}
-
-	thumb := image.NewNRGBA(image.Rect(0, 0, newWidth, newHeight))
+	thumb := image.NewNRGBA(thumbDimensions(img.Bounds(), maxWidth, maxHeight))
 	draw.BiLinear.Scale(thumb, thumb.Bounds(), img, img.Bounds(), draw.Over, nil)
 
 	return thumb, nil
+}
+
+// ThumbnailFile is a convenience method for retrieving a thumbnail directly on a file using Thumbnail.
+func ThumbnailFile(path string, maxWidth, maxHeight int) (image.Image, error) {
+	pc, err := ReadFile(path, false)
+	if err != nil {
+		return nil, err
+	}
+
+	return Thumbnail(pc, maxWidth, maxHeight)
+}
+
+func thumbDimensions(b image.Rectangle, maxWidth, maxHeight int) image.Rectangle {
+	if maxWidth == 0 {
+		maxWidth = b.Dx()
+	}
+	if maxHeight == 0 {
+		maxHeight = b.Dy()
+	}
+
+	newWidth := maxWidth
+	newHeight := b.Dy() * maxWidth / b.Dx()
+
+	if newHeight > maxHeight {
+		newHeight = maxHeight
+		newWidth = b.Dx() * maxHeight / b.Dy()
+	}
+
+	return image.Rect(0, 0, newWidth, newHeight)
 }
---
+++ thumbnail_test.go
@@ -0,0 +1,26 @@
+package postcard_test
+
+import (
+	"bytes"
+	"crypto/md5"
+	"fmt"
+	"image/png"
+
+	"github.com/jphastings/postcard-go"
+)
+
+func ExampleThumbnailFile() {
+	thumb, err := postcard.ThumbnailFile("fixtures/hello.postcard", 128, 0)
+	if err != nil {
+		panic(err)
+	}
+
+	buf := new(bytes.Buffer)
+	if err := png.Encode(buf, thumb); err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("Thumbnail PNG (%dx%d) has checksum %x",
+		thumb.Bounds().Dx(), thumb.Bounds().Dy(), md5.Sum(buf.Bytes()))
+	// Output: Thumbnail PNG (128x157) has checksum a611eabdf866dde24137210963bf5b91
+}
