4290ef82c18ca68f78fc3ab8041ff6b7eaeb500a
JP Hastings-Spital <jphastings@gmail.com>
2023-12-16 10:37:03 +0000

    Break out webp encoder

--- compile/compile.go
+++ compile/compile.go
@@ -15,9 +15,6 @@ import (
 	"github.com/dotpostcard/postcards-go"
 	"github.com/dotpostcard/postcards-go/internal/types"
 	"github.com/dotpostcard/postcards-go/validate"
-	"github.com/kolesa-team/go-webp/encoder"
-	"github.com/kolesa-team/go-webp/webp"
-	"github.com/kolesa-team/goexiv"
 )
 
 var nameRegex = regexp.MustCompile(`(.+)-(?:front|back|meta)+\.[a-z]+`)
@@ -189,33 +186,6 @@ func metaReader(r io.Reader, ext string) (MetadataProvider, error) {
 	default:
 		return nil, fmt.Errorf("unknown metadata format: %s", ext)
 	}
-}
-
-var webpEncoderOpts *encoder.Options
-
-func init() {
-	opts, err := encoder.NewLossyEncoderOptions(encoder.PresetDefault, 85)
-	if err != nil {
-		panic(err)
-	}
-	webpEncoderOpts = opts
-}
-
-// encodeWebp turns a the image.Image into bytes in Webp format. Currently does *not* write the resolution
-// bytes into exif tags, as I can't find a good library for completing this (goexiv doesn't support writing
-// rational numbers, which XResolution and YResolution are.)
-func encodeWebp(img image.Image, size types.Size) ([]byte, error) {
-	data := new(bytes.Buffer)
-	if err := webp.Encode(data, img, webpEncoderOpts); err != nil {
-		return nil, err
-	}
-
-	goIm, err := goexiv.OpenBytes(data.Bytes())
-	if err != nil {
-		return nil, err
-	}
-
-	return goIm.GetBytes(), nil
 }
 
 var oversized float64 = 30 // Centimetres
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/dotpostcard/postcards-go
 go 1.19
 
 require (
+	github.com/chai2010/webp v1.1.1
 	github.com/fogleman/gg v1.3.0
 	github.com/spf13/cobra v1.5.0
 	golang.org/x/image v0.0.0-20220902085622-e7cb96979f69
